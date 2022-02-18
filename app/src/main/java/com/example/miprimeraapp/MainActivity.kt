59797d3dbcfa7d04a96ac6b30fb092d9ecabfc59
JazminLoM <94807623+JazminLoM@users.noreply.github.com>
2022-02-17 19:15:54 -0600

    Revert: Uniendo Vista con Codigo

--- app/src/main/java/com/example/miprimeraapp/MainActivity.kt
+++ app/src/main/java/com/example/miprimeraapp/MainActivity.kt
@@ -2,20 +2,12 @@ package com.example.miprimeraapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val bSaludar = findViewById<Button>(R.id.bSaludar)
-        bSaludar.setOnClickListener(View.OnClickListener {
-            Toast.makeText(this, "Hola!", Toast.LENGTH_LONG).show()
-        })
     }
     fun validaDato(): Boolean{
         val etNombre = findViewById<EditText>(R.id.etNombre)
