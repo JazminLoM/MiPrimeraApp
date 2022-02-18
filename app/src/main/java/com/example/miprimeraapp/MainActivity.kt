e2ad5e25e79ba4a6c5d723325d5e0dac3a916037
JazminLoM <94807623+JazminLoM@users.noreply.github.com>
2022-02-17 18:49:54 -0600

    Inicio de App

---
+++ app/src/main/java/com/example/miprimeraapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.miprimeraapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun validaDato(): Boolean{
+        val etNombre = findViewById<EditText>(R.id.etNombre)
+        val nombreUsuario = etNombre.text
+
+        if(nombreUsuario.isNullOrEmpty()){
+            return false
+        }
+        return true
+    }
+}
